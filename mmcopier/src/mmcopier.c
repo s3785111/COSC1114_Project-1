78109d0ee71e4a6a80932786fa3ba120c827ee38
Matthew Ricci <s3785111@student.rmit.edu.au>
2024-08-27 22:59:35 +1000

    Improve error checking, implement early file handling, minor formatting adjustments

--- mmcopier/src/mmcopier.c
+++ mmcopier/src/mmcopier.c
@@ -1,7 +1,7 @@
 /***********************************************************************************
  * @file        mmcopier.c                                                         *
  * @author      Matt Ricci                                                         *
- * @brief       Multithreaded single-file copying utility.                         *
+ * @brief       Multithreaded multi-file copying utility.                          *
  ***********************************************************************************/
 
 #include "mmcopier.h"
@@ -18,11 +18,11 @@
  * =============================================================================== */
 int mmcopier_parse(int argc, char *argv[], int *n) {
   if (argc != ARGC) {
-    printf("Argument Error: %s expected %d inputs, received %d.\n", argv[0], ARGC, argc);
+    printf("Argument Error: Expected %d inputs, received %d.\n", ARGC, argc);
     return 1;
   }
   if (sscanf(argv[1], "%d", n) != 1) {
-    printf("Arugment Error: %s expects first input parameter to be integer type.\n", argv[0]);
+    printf("Arugment Error: Expected first input parameter to be integer type.\n");
     return 1;
   }
   if (*n > SIZE_N || *n < 1) {
@@ -32,25 +32,69 @@ int mmcopier_parse(int argc, char *argv[], int *n) {
   return 0;
 }
 
-void *mmcopier_copy() {
-  printf("foo");
+void *mmcopier_copy(void *param) {
+  FILE *infile  = fopen((char *)param, "r");
+  FILE *outfile = fopen((char *)param, "w+");
   pthread_exit(0);
 }
 
 int main(int argc, char *argv[]) {
 
+  // Parse input parameters, early exit on error
   int n, ret;
   if ((ret = mmcopier_parse(argc, argv, &n)))
     return ret;
 
-  for (int i = 0; i < n; i++) {
+  // Retrieve string representation of working directory
+  char workingDir[FILENAME_MAX];
+  char srcDir[FILENAME_MAX];
+  char destDir[FILENAME_MAX];
+  getcwd(workingDir, FILENAME_MAX);
+
+  // ============================
+  //       ERROR CHECKING
+  // ============================
+
+  // Concatenate source directory path and check length of pathname
+  size_t fileLength = snprintf(srcDir, FILENAME_MAX, "%s/%s", workingDir, argv[2]);
+  if (fileLength > sizeof(srcDir)) {
+    printf("Path Error: Source path too long\n");
+    return 1;
+  }
+
+  // Concatenate destination directory path and check length of pathname
+  fileLength = snprintf(destDir, FILENAME_MAX, "%s/%s", workingDir, argv[3]);
+  if (fileLength > sizeof(destDir)) {
+    printf("Path Error: Destination path too long\n");
+    return 1;
+  }
+
+  // Read files from source directory and ensure it exists
+  struct dirent **files;
+  if (scandir(srcDir, &files, NULL, alphasort) < n) {
+    printf("Path error: Insufficient files or directory %s does not exist\n", srcDir);
+    return 1;
+  }
+
+  // ============================
+  //       THREAD HANDLING
+  // ============================
+
+  // Copy source and destination directories to args struct
+  mmcopier_args args;
+  strcpy(args.destDir, destDir);
+  strcpy(args.srcDir, srcDir);
+
+  // Loop through files ignoring . and ..
+  for (int i = 2; i < 2 + n; i++) {
     pthread_t tid;
     pthread_attr_t attr;
-    // @todo read in files from parsed directory argument
-    // @todo pass in unique files to be copied into spawned thread
-    pthread_attr_init(&attr);                         // Initialise attributes
-    pthread_create(&tid, &attr, mmcopier_copy, NULL); // Spawn thread for file copy
-    pthread_join(tid, NULL);                          // Join main thread to child
+
+    // Spawn thread for file and execute copy
+    pthread_attr_init(&attr);                          // Initialise attributes
+    strcpy(args.fileName, files[i]->d_name);           // Copy input file name to args struct
+    pthread_create(&tid, &attr, mmcopier_copy, &args); // Spawn thread for file copy and pass in filename
+    pthread_join(tid, NULL);                           // Join main thread to child
   }
 
   return 0;
--- mmcopier/src/mmcopier.h
+++ mmcopier/src/mmcopier.h
@@ -7,14 +7,23 @@
 #ifndef _MMCOPIER_H
 #define _MMCOPIER_H
 
+#include "dirent.h"
 #include "pthread.h"
 #include "stdint.h"
 #include "stdio.h"
+#include "string.h"
+#include "unistd.h"
 
 #define ARGC   4
 #define SIZE_N 10
 
+typedef struct {
+  char fileName[FILENAME_MAX];
+  char srcDir[FILENAME_MAX];
+  char destDir[FILENAME_MAX];
+} mmcopier_args;
+
 int mmcopier_parse(int, char *[], int *);
-void *mmcopier_copy();
+void *mmcopier_copy(void *param);
 
 #endif
